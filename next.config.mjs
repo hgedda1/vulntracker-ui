aec4c1357580611512ea4e16ec9a93f9c0a5da43
hemanth-kdex <hemanth@kdexglobal.com>
2025-07-15 13:17:29 -0500

    vercel-2

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  output: 'export',
   basePath: isProd ? '/vulntracker-ui' : '',
   assetPrefix: isProd ? '/vulntracker-ui/' : '',
   images: {
